48cac62e34fea3d3825b4cfa6deab4727d6fe916
Myoungmin <miung222@gmail.com>
2022-06-30 17:22:20 +0900

    이미지 처리에서 여러 효과를 동시에 적용하는 실습 추가

---
+++ MultipleEffects/WebGL_MultipleEffects.js
@@ -0,0 +1,363 @@
+"use strict";
+
+function main() {
+  var image = new Image();
+  image.src = "../cathedral.jpg";  // MUST BE SAME DOMAIN!!!
+  image.onload = function() {
+    render(image);
+  };
+}
+
+function render(image) {
+  // Get A WebGL context
+  /** @type {HTMLCanvasElement} */
+  var canvas = document.querySelector("#canvas");
+  var gl = canvas.getContext("webgl");
+  if (!gl) {
+    return;
+  }
+
+  // setup GLSL program
+  var program = webglUtils.createProgramFromScripts(gl, ["vertex-shader-2d", "fragment-shader-2d"]);
+
+  // look up where the vertex data needs to go.
+  var positionLocation = gl.getAttribLocation(program, "a_position");
+  var texcoordLocation = gl.getAttribLocation(program, "a_texCoord");
+
+  // Create a buffer to put three 2d clip space points in
+  var positionBuffer = gl.createBuffer();
+  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  // Set a rectangle the same size as the image.
+  setRectangle( gl, 0, 0, image.width, image.height);
+
+  // provide texture coordinates for the rectangle.
+  var texcoordBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+      0.0,  0.0,
+      1.0,  0.0,
+      0.0,  1.0,
+      0.0,  1.0,
+      1.0,  0.0,
+      1.0,  1.0,
+  ]), gl.STATIC_DRAW);
+
+  // 텍스처 생성 함수
+  function createAndSetupTexture(gl) {
+    var texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+    // // 텍스처를 설정하여 어떤 크기의 이미지도 렌더링할 수 있도록 하고 픽셀로 작업한다.
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    return texture;
+  }
+
+  // 텍스처를 만들고 이미지를 넣는다.
+  var originalImageTexture = createAndSetupTexture(gl);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
+  // 텍스처 2개를 만들고 프레임 버퍼에 첨부한다.
+  var textures = [];
+  var framebuffers = [];
+  for (var ii = 0; ii < 2; ++ii) {
+    var texture = createAndSetupTexture(gl);
+    textures.push(texture);
+
+    // 이미지와 같은 크기로 텍스처 만들기
+    gl.texImage2D(
+        gl.TEXTURE_2D, 0, gl.RGBA, image.width, image.height, 0,
+        gl.RGBA, gl.UNSIGNED_BYTE, null);
+
+    // 프레임 버퍼 생성
+    var fbo = gl.createFramebuffer();
+    framebuffers.push(fbo);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+
+    // 텍스처 첨부
+    gl.framebufferTexture2D(
+        gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+  }
+
+  // lookup uniforms
+  var resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+  var textureSizeLocation = gl.getUniformLocation(program, "u_textureSize");
+  var kernelLocation = gl.getUniformLocation(program, "u_kernel[0]");
+  var kernelWeightLocation = gl.getUniformLocation(program, "u_kernelWeight");
+  var flipYLocation = gl.getUniformLocation(program, "u_flipY");
+
+  // Define several convolution kernels
+  var kernels = {
+    normal: [
+      0, 0, 0,
+      0, 1, 0,
+      0, 0, 0
+    ],
+    gaussianBlur: [
+      0.045, 0.122, 0.045,
+      0.122, 0.332, 0.122,
+      0.045, 0.122, 0.045
+    ],
+    gaussianBlur2: [
+      1, 2, 1,
+      2, 4, 2,
+      1, 2, 1
+    ],
+    gaussianBlur3: [
+      0, 1, 0,
+      1, 1, 1,
+      0, 1, 0
+    ],
+    unsharpen: [
+      -1, -1, -1,
+      -1,  9, -1,
+      -1, -1, -1
+    ],
+    sharpness: [
+       0,-1, 0,
+      -1, 5,-1,
+       0,-1, 0
+    ],
+    sharpen: [
+       -1, -1, -1,
+       -1, 16, -1,
+       -1, -1, -1
+    ],
+    edgeDetect: [
+       -0.125, -0.125, -0.125,
+       -0.125,  1,     -0.125,
+       -0.125, -0.125, -0.125
+    ],
+    edgeDetect2: [
+       -1, -1, -1,
+       -1,  8, -1,
+       -1, -1, -1
+    ],
+    edgeDetect3: [
+       -5, 0, 0,
+        0, 0, 0,
+        0, 0, 5
+    ],
+    edgeDetect4: [
+       -1, -1, -1,
+        0,  0,  0,
+        1,  1,  1
+    ],
+    edgeDetect5: [
+       -1, -1, -1,
+        2,  2,  2,
+       -1, -1, -1
+    ],
+    edgeDetect6: [
+       -5, -5, -5,
+       -5, 39, -5,
+       -5, -5, -5
+    ],
+    sobelHorizontal: [
+        1,  2,  1,
+        0,  0,  0,
+       -1, -2, -1
+    ],
+    sobelVertical: [
+        1,  0, -1,
+        2,  0, -2,
+        1,  0, -1
+    ],
+    previtHorizontal: [
+        1,  1,  1,
+        0,  0,  0,
+       -1, -1, -1
+    ],
+    previtVertical: [
+        1,  0, -1,
+        1,  0, -1,
+        1,  0, -1
+    ],
+    boxBlur: [
+        0.111, 0.111, 0.111,
+        0.111, 0.111, 0.111,
+        0.111, 0.111, 0.111
+    ],
+    triangleBlur: [
+        0.0625, 0.125, 0.0625,
+        0.125,  0.25,  0.125,
+        0.0625, 0.125, 0.0625
+    ],
+    emboss: [
+       -2, -1,  0,
+       -1,  1,  1,
+        0,  1,  2
+    ]
+  };
+
+  var effects = [
+    { name: "gaussianBlur3", on: true },
+    { name: "gaussianBlur3", on: true },
+    { name: "gaussianBlur3", on: true },
+    { name: "sharpness", },
+    { name: "sharpness", },
+    { name: "sharpness", },
+    { name: "sharpen", },
+    { name: "sharpen", },
+    { name: "sharpen", },
+    { name: "unsharpen", },
+    { name: "unsharpen", },
+    { name: "unsharpen", },
+    { name: "emboss", on: true },
+    { name: "edgeDetect", },
+    { name: "edgeDetect", },
+    { name: "edgeDetect3", },
+    { name: "edgeDetect3", },
+  ];
+
+  // Setup a ui.
+  var ui = document.querySelector("#ui");
+  var table = document.createElement("table");
+  var tbody = document.createElement("tbody");
+  for (var ii = 0; ii < effects.length; ++ii) {
+    var effect = effects[ii];
+    var tr = document.createElement("tr");
+    var td = document.createElement("td");
+    var chk = document.createElement("input");
+    chk.value = effect.name;
+    chk.type = "checkbox";
+    if (effect.on) {
+      chk.checked = "true";
+    }
+    chk.onchange = drawEffects;
+    td.appendChild(chk);
+    td.appendChild(document.createTextNode('≡ ' + effect.name));
+    tr.appendChild(td);
+    tbody.appendChild(tr);
+  }
+  table.appendChild(tbody);
+  ui.appendChild(table);
+  $(table).tableDnD({onDrop: drawEffects});
+
+  drawEffects();
+
+  function computeKernelWeight(kernel) {
+    var weight = kernel.reduce(function(prev, curr) {
+        return prev + curr;
+    });
+    return weight <= 0 ? 1 : weight;
+  }
+
+  function drawEffects(name) {
+    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+    // Clear the canvas
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // Tell it to use our program (pair of shaders)
+    gl.useProgram(program);
+
+    // Turn on the position attribute
+    gl.enableVertexAttribArray(positionLocation);
+
+    // Bind the position buffer.
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    // Tell the position attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    var size = 2;          // 2 components per iteration
+    var type = gl.FLOAT;   // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0;        // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+        positionLocation, size, type, normalize, stride, offset);
+
+    // Turn on the texcoord attribute
+    gl.enableVertexAttribArray(texcoordLocation);
+
+    // bind the texcoord buffer.
+    gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
+
+    // Tell the texcoord attribute how to get data out of texcoordBuffer (ARRAY_BUFFER)
+    var size = 2;          // 2 components per iteration
+    var type = gl.FLOAT;   // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0;        // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+        texcoordLocation, size, type, normalize, stride, offset);
+
+    // set the size of the image
+    gl.uniform2f(textureSizeLocation, image.width, image.height);
+
+    // 원본 이미지로 시작
+    gl.bindTexture(gl.TEXTURE_2D, originalImageTexture);
+
+    // 텍스처에 그리는 동안 이미지 y축 뒤집지 않기
+    gl.uniform1f(flipYLocation, 1);
+
+    // 우리가 적용하고 싶은 효과 반복
+    var count = 0;
+    for (var ii = 0; ii < tbody.rows.length; ++ii) {
+      var checkbox = tbody.rows[ii].firstChild.firstChild;
+      if (checkbox.checked) {
+        // 프레임 버퍼 중 하나에 그리도록 설정한다.
+        setFramebuffer(framebuffers[count % 2], image.width, image.height);
+
+        drawWithKernel(checkbox.value);
+
+        // 다음 그리기를 위해, 방금 렌더링한 텍스처를 사용합니다.
+        gl.bindTexture(gl.TEXTURE_2D, textures[count % 2]);
+
+        // 다음 순서로 변경하도록 인덱스 변경
+        ++count;
+      }
+    }
+
+    // 마지막으로 결과를 캔버스에 그린다.
+    gl.uniform1f(flipYLocation, -1);  // 캔버스 y축 뒤집기 필요
+    setFramebuffer(null, gl.canvas.width, gl.canvas.height);
+    drawWithKernel("normal");
+  }
+
+  function setFramebuffer(fbo, width, height) {
+    // 이걸 렌더링할 프레임 버퍼로 만든다.
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+
+    // 프레임 버퍼의 해상도를 셰이더에 알려준다.
+    gl.uniform2f(resolutionLocation, width, height);
+
+    // WebGL에 프레임 버퍼에 필요한 뷰포트 설정을 알려준다.
+    gl.viewport(0, 0, width, height);
+  }
+
+
+  function drawWithKernel(name) {
+    // 커널 설정
+    gl.uniform1fv(kernelLocation, kernels[name]);
+    // 가중치 설정
+    gl.uniform1f(kernelWeightLocation, computeKernelWeight(kernels[name]));
+
+    // 사각형을 그린다.
+    var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    var count = 6;
+    gl.drawArrays(primitiveType, offset, count);
+  }
+}
+
+function setRectangle(gl, x, y, width, height) {
+  var x1 = x;
+  var x2 = x + width;
+  var y1 = y;
+  var y2 = y + height;
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+     x1, y1,
+     x2, y1,
+     x1, y2,
+     x1, y2,
+     x2, y1,
+     x2, y2,
+  ]), gl.STATIC_DRAW);
+}
+
+main();
